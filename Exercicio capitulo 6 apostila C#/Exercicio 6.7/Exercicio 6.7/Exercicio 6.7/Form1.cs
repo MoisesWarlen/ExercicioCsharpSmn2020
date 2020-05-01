eefddfe4c7dfd4053f3eb21466c79f0e2ce824c3
Moises warlen <moises.warlen@smn.com.br>
2020-04-30 22:32:20 -0300

    Primeiro exercicio orientado a objeto da apostila

---
+++ Exercicio capitulo 6 apostila C#/Exercicio 6.7/Exercicio 6.7/Exercicio 6.7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_6._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            conta contaVitor = new conta(); // declarando novo objeto
+            contaVitor.titular = "Vitor";
+            contaVitor.numero = 1;       
+            contaVitor.idade=21;
+            
+
+            if (contaVitor.saca(30))  //validação para sacar
+            {
+                MessageBox.Show("Saque realizado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Erro ao sacar ");
+            }
+
+
+        }
+    }
+}
---
+++ Exercicio capitulo 6 apostila C#/Exercicio 6.7/Exercicio 6.7/Exercicio 6.7/conta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_6._7
+{
+    class conta
+    {
+
+         public double saldo =1000;
+         public int numero ,idade;
+         public string titular;
+        
+
+        public void Deposita(double valor)
+        {
+
+           this. saldo += valor;
+
+        }
+        public bool saca(double valor)
+        {
+            if ((this.saldo >= valor) && (valor > 0))//se tiver dinheiro
+            {
+                if (this.idade >= 18)//se for de maior 
+                {
+
+                    this.saldo -= valor;//saca qualquer valor 
+                    return true;//falo q deu certo 
+                }
+
+                else//se for menor de 18
+                {
+                    if (valor >= 200)//verifico se vai sacar mais de 200 R$
+                    {
+                        return false;//nao deixo sacar mais de 200 R$
+                    }
+                    this.saldo -= valor;//se for menos de 200
+                    return true;//falo q deu certo 
+                }
+            }
+            return false;//se nao tiver dinheiro
+        }
+
+
+        public void tranfere(double valor ,conta destino)
+        {
+            this.saca(valor);
+            destino.Deposita(valor);
+            
+        }
+
+    }
+
+
+}
