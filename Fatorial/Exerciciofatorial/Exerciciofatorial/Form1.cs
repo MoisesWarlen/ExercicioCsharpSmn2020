b5e661a5b77e60b3648a8f33ecb47331c7fe2695
Moises warlen <moises.warlen@smn.com.br>
2020-04-30 22:10:35 -0300

    Exercicio   fatorial

---
+++ Fatorial/Exerciciofatorial/Exerciciofatorial/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exerciciofatorial
+{
+    public partial class Form1 : Form
+    {
+        int fatorial = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int n =1 ;n <=10 ;n++)
+            {
+                fatorial *= n;
+                MessageBox.Show(""+n+"fatorial ="+fatorial);
+
+            }
+        }
+    }
+}
