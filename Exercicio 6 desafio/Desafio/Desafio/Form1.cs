83effd470e5a2218b1ae01e983f9d432806906ae
Moises warlen <moises.warlen@smn.com.br>
2020-04-30 22:28:09 -0300

    Exercicio desafio da apostila

---
+++ Exercicio 6 desafio/Desafio/Desafio/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio
+{
+    public partial class Form1 : Form
+    {  
+        int valor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            valor = int.Parse(txtNumero.Text);
+
+            bool verifica = (valor > 10); MessageBox.Show("numero  digitado  e maior ? "+verifica);
+
+
+        }
+    }
+}
