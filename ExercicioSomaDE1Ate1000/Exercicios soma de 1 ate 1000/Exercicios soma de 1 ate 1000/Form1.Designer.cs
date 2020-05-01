27fa656e1b2e9f4e1e11bb83498cbc2342f69945
Moises warlen <moises.warlen@smn.com.br>
2020-04-30 22:25:57 -0300

    Exercicio que soma os numeros entre 1 e 1000

---
+++ ExercicioSomaDE1Ate1000/Exercicios soma de 1 ate 1000/Exercicios soma de 1 ate 1000/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Exercicios_soma_de_1_ate_1000
+{
+    partial class somaDe1Ate1000
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSoma = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(198, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "somar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // lblSoma
+            // 
+            this.lblSoma.AutoSize = true;
+            this.lblSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoma.Location = new System.Drawing.Point(308, 15);
+            this.lblSoma.Name = "lblSoma";
+            this.lblSoma.Size = new System.Drawing.Size(122, 29);
+            this.lblSoma.TabIndex = 2;
+            this.lblSoma.Text = "resultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soma dos numero entre 1 e 1000 =";
+            // 
+            // somaDe1Ate1000
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 120);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSoma);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "somaDe1Ate1000";
+            this.Text = "somaDe1Ate1000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSoma;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExercicioSomaDE1Ate1000/Exercicios soma de 1 ate 1000/Exercicios soma de 1 ate 1000/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios_soma_de_1_ate_1000
+{
+    public partial class somaDe1Ate1000 : Form
+    {
+        int total;
+        public somaDe1Ate1000()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i =0 ;i <=1000 ;i ++)
+            {
+                total = total + i;
+             
+            }
+            lblSoma.Text = total.ToString();
+        }
+    }
+}
