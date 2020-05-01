12c24fb1157d45569e4a286171ff83fa49aaf6d5
Moises warlen <moises.warlen@smn.com.br>
2020-04-30 22:19:22 -0300

    Exercicio exibindo numeros multiplos de 3 entre 1 e 100

---
+++ ExercicioMultiploDe3Ate100/Multiplo de 3 a 100/Multiplo de 3 a 100/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Multiplo_de_3_a_100
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblVet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ver numeros multiplos de 3";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblVet
+            // 
+            this.lblVet.AutoSize = true;
+            this.lblVet.Location = new System.Drawing.Point(96, 90);
+            this.lblVet.Name = "lblVet";
+            this.lblVet.Size = new System.Drawing.Size(35, 13);
+            this.lblVet.TabIndex = 1;
+            this.lblVet.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblVet);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Multiplas de 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblVet;
+    }
+}
+
---
+++ ExercicioMultiploDe3Ate100/Multiplo de 3 a 100/Multiplo de 3 a 100/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplo_de_3_a_100
+{
+    public partial class Form1 : Form
+    {
+        int multiplosDeTres, cont,a ,b,qtdMult;
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            qtdMult = 0;
+           for (int i =1;i <=100;i++)
+            {
+                if(i % 3 == 0)
+                {
+                    qtdMult++;
+                }
+            }
+            int[] vet = new int[qtdMult];
+            cont = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    vet[cont] = i;
+                    cont++;
+                }
+            }
+            for (int i = 0; i <cont;i++)
+            {
+
+                lblVet.Text = ($"vet[{i}] = {vet[i]}");
+                MessageBox.Show(vet[i].ToString());
+            }
+           
+        }
+    }
+}
