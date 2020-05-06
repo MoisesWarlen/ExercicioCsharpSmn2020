9b09a2e28040fa0c3eda521ef27cbbc2036b65b0
Moises warlen <moises.warlen@smn.com.br>
2020-05-06 13:01:32 -0300

    Exercicio 9 Banco INTRODUÇÃO AO VISUAL STUDIO COM

---
+++ Exercicio9 Banco/Banco/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace Banco
+{
+    internal class Cliente //criando a classe
+    {
+       
+     public string Nome { get; private set; } //declarando variaveis
+        public Cliente(string nome) //contrutor 
+        {
+            this.Nome = nome;
+        }
+
+      
+    }
+}
