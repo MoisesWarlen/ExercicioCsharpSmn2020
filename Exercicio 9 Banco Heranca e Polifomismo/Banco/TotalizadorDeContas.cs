7cf9a7af9e16ae17c6a4deb9d3a504e743b27a06
Moises warlen <moises.warlen@smn.com.br>
2020-05-06 21:10:53 -0300

    Exercicio sobre Herança e polifomismo

---
+++ Exercicio 9 Banco Heranca e Polifomismo/Banco/TotalizadorDeContas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class TotalizadorDeContas
+    {
+
+        public double SaldoTotal { get; set; }
+        public void  Adiciona (Conta totalizador)
+        {
+
+            this.SaldoTotal += totalizador.Saldo; 
+              
+        }
+
+    }
+}
